94f5063642dfbb5089ea528a35a556f8238efb74
01Qin <sisukasqin@hotmail.com>
2023-09-15 21:51:13 +0800

    area changed

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 import math
 radius = float(input('Enter the radius of a circle:'))
 PI = 3.14
-area = ('PI * radius * radius')
-print( "The area of the circle is: " "(area) ")
+area = PI * radius * radius
+print ("The area of the circle is:", area)
