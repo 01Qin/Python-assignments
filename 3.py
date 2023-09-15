7fbeed7d99ed03b9d0698c5fb89acb82d52fea84
01Qin <sisukasqin@hotmail.com>
2023-09-15 22:00:17 +0800

    area changed

---
+++ 3.py
@@ -0,0 +1,5 @@
+length = input('the length of a rectangle:')
+width = input('the width of a rectangle:')
+area = length * width
+perimeter = 2 * length + 2 * width
+print('the area of a rectangle is: ', area ,'the perimeter of a rectangle is:', perimeter)
