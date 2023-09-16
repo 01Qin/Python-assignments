83bc7b00cc43ec1b84c8f38d21b7ba656ee6c7e8
01Qin <sisukasqin@hotmail.com>
2023-09-16 18:03:58 +0800

    area changed

--- 4.py
+++ 4.py
@@ -1,8 +1,13 @@
 import math
-Number = input(int("Enter three numbers:"))
-if Number > 0:
-    print("The sum is:", area)
-    print("The average is:", area)
-    print("The sum is:", area)
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+sum = number1 + number2 + number3
+average = sum / 3
+product = number1 * number2 * number3
+if (number1, number2 , number3).sort()
+    print("The sum is:", sum)
+    print("The average is:", average)
+    print("The product is:", product)
 else: \
     (print("please input integers."))
--- 5.py
+++ 5.py
@@ -6,4 +6,4 @@ x = 20 * y
 y = 32 * z
 a = z * 13.3 gram
 b = 1000 * a
-print("The weight in modern units: ", a, b)
+print("The weight in modern units: ", a 'and' b)
