d1b97d36e82d54c2073b74f5c62dbb41942fa5ab
01Qin <sisukasqin@hotmail.com>
2023-09-16 20:29:31 +0800

    type changed

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
 import math
-length = input('the length of a rectangle:')
-width = input('the width of a rectangle:')
+length = float(input('the length of a rectangle:'))
+width = float(input('the width of a rectangle:'))
 area = length * width
 perimeter = 2 * length + 2 * width
 if length > 0 and width > 0:
     print("The area of the rectangle is:", area)
     print("The perimeter of the rectangle is:", perimeter)
-else: \
+else:
     (print("wrong numbers."))
