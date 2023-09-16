7afd76313d3742f1aaccbf889eacd77309256283
01Qin <sisukasqin@hotmail.com>
2023-09-16 17:38:34 +0800

    area changed

--- 5.py
+++ 5.py
@@ -1,3 +1,9 @@
-talents = input("Enter talents:")
-pounds = input("Enter pounds:")
-lots = input("Enter lots:")
+import gram
+x = input("Enter talents:")
+y = input("Enter pounds:")
+z = input("Enter lots:")
+x = 20 * y
+y = 32 * z
+a = z * 13.3 gram
+b = 1000 * a
+print("The weight in modern units: ", a, b)
