95aa5c82dc628d9f7445b4ff46fb3a71be74f78d
01Qin <sisukasqin@hotmail.com>
2023-09-16 21:14:25 +0800

    type changed

--- 5.py
+++ 5.py
@@ -5,5 +5,5 @@ z = float(input("Enter lots:"))
 x = 20 * y
 a = z * 13.3
 y = 32 * z
-b = 1000 * a
-print("The weight in modern units: ", a and b)
+b = a / 1000
+print(f"The weight in modern units: {(a and b):.2f} grams")
