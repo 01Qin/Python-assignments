74ccecb71bd83f666b3496d466d1fe523aa18eac
01Qin <sisukasqin@hotmail.com>
2023-09-17 14:54:13 +0800

    type changed

--- 5.py
+++ 5.py
@@ -2,8 +2,8 @@ import math
 talents = float(input("Enter talents:"))
 pounds = float(input("Enter pounds:"))
 lots = float(input("Enter lots:"))
-talents = float(20 * pounds)
-pounds = float(32 * lots)
-grams = float(13.3 * float(lots))
+s_pounds = pounds + talents * 20
+s_lots = lots + s_pounds * 32
+grams = float(13.3 * float(s_lots))
 kilograms = float(grams / 1000)
 print(f"The weight in modern units: {kilograms:.2f} kilograms and {grams:.2f} grams")
