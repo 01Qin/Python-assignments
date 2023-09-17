e578f3407cdc6f458e44850dd851580de09d070f
01Qin <sisukasqin@hotmail.com>
2023-09-17 15:05:40 +0800

    type changed

--- 2.py
+++ 2.py
@@ -4,5 +4,5 @@ radius = float(input('Enter the radius of a circle:'))
 area = PI * radius * radius
 if radius > 0:
     print("The area of the circle is:", area)
-else: \
+else:
     (print("wrong radius."))
