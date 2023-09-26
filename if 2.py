54389f291c8274b46ecc67acb0195e9f5d1b7ab0
01Qin <sisukasqin@hotmail.com>
2023-09-26 21:23:25 +0800

    changed1

--- if 2.py
+++ if 2.py
@@ -1,11 +1,11 @@
 c = str(input("enter the cabin class of a cruise ship:"))
 if c == "LUX":
     print("upper-deck cabin with a balcony.")
-if c == "A":
-    print("above the car deck, equipped with a window")
-if c == "B":
+elif c == "A":
+    print("above the car deck, equipped with a window.")
+elif c == "B":
     print("windowless cabin above the car deck.")
-if c == "C":
+elif c == "C":
     print("windowless cabin below the car deck.")
 else:
     print("Invalid cabin class.")
