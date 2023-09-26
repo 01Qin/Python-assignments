6990ee6431f5898cb43f309f672c7862a13c5601
01Qin <sisukasqin@hotmail.com>
2023-09-26 21:11:59 +0800

    changed1

---
+++ if 2.py
@@ -0,0 +1,11 @@
+class = str(input("enter the cabin class of a cruise ship:"))
+if class == LUX
+    print("upper-deck cabin with a balcony.")
+if class == A
+    print("above the car deck, equipped with a window")
+if class == B
+    print("windowless cabin above the car deck.")
+if class == C
+    print("windowless cabin below the car deck.")
+eles://
+print("Invalid cabin class.')
