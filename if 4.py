175f386bd2dac1c6af63aac41e75023c2e6d4d2f
01Qin <sisukasqin@hotmail.com>
2023-09-27 20:23:08 +0800

    changed1

--- if 4.py
+++ if 4.py
@@ -1,5 +1,7 @@
 year = int(input("Enter a year:"))
 if year / 4 == 0:
     print("The input year is a leap year.")
+    elif year / 100 == 0:
+    print("The input year is a leap year.")
 else:
     print("The input year is not a leap year.")
