bcc7a85791aee9edbb34ab40283e75a27f75f97c
01Qin <sisukasqin@hotmail.com>
2023-09-27 09:21:42 +0800

    changed1

--- if 3.py
+++ if 3.py
@@ -1,2 +1,15 @@
 gender = input("the biological gender")
-hemo = input("the hemoglobin value")
+hemo = float(input("the hemoglobin value"))
+if gender = "female" and "117" >= hemo <="155":
+    print("A normal hemoglobin value")
+elif hemo<117:
+    print("A low hemoglobin value")
+elif hemo>155:
+    print("A high hemoglobin value")
+if gender = "male" and "134"<=hemo<="167":
+    print("A normal hemoglobin value")
+elif hemo<134:
+    print("A low hemoglobin value")
+elif hemo>167:
+    print("A low hemoglobin value")
+
