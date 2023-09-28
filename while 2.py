f700441c6a8fb4cb8370fb7d8ab3ae22143aa86a
01Qin <sisukasqin@hotmail.com>
2023-09-28 16:19:24 +0800

    changed1

---
+++ while 2.py
@@ -0,0 +1,5 @@
+value = int(input("enter a value:"))
+while value >= 0:
+    print(f"executing value:", value * 2.54, "centimeters")
+    value = int(input("enter a value:"))
+print("execution stopped")
