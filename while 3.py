41094416f09e742468f853fd7ed909f37d47802f
01Qin <sisukasqin@hotmail.com>
2023-09-30 22:22:12 +0800

    changed1

--- while 3.py
+++ while 3.py
@@ -1,6 +1,13 @@
 import random
-num = [1,10]
+num = random.randint[1-10]
 input_num = int(input("enter a number:"))
-wrong_num = num - input_num
+wrong_num = int(num) - input_num
 while input_num != num:
-    if
+    if wrong_num > 0:
+        print(f"too high",{int(num) - input_num})
+    else:
+        print(f"too low",{int(num) - input_num})
+else:
+    print("correct number")
+
+
