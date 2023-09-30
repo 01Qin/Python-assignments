296dda45548e8f3cdca8a01135b80d89b00fd481
01Qin <sisukasqin@hotmail.com>
2023-09-30 21:05:36 +0800

    changed1

---
+++ while 3.py
@@ -0,0 +1,11 @@
+number = float(input("enter a number:"))
+smallest = min(number)
+largest = max(number)
+while number == "":
+    break
+else:
+    if number:
+
+    print("the smallest number is:" + smallest_number)
+    print("the largest number is:" + largest_number)
+    print("Execution stopped.")
