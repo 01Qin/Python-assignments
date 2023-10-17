969c91ee1b40e836a7eae7d612f98997e0916e57
01Qin <sisukasqin@hotmail.com>
2023-10-17 16:32:10 +0800

    changed1

--- if 1.py
+++ if 1.py
@@ -1,6 +1,6 @@
-length = float(input('enter the length of a zander:'))
+length = float(input("enter the length of a zander:"))
 if length < 42:
-    print('release the fish back into the lake')
+    print("release the fish back into the lake")
     print(f"below the size limit the caught fish was: { 42 - length }")
 else:
     print("a true zander")
