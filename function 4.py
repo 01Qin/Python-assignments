3ad98033625d9a172e8057c79ad5448cc76c0883
01Qin <sisukasqin@hotmail.com>
2023-10-19 19:36:57 +0800

    changed1

--- function 4.py
+++ function 4.py
@@ -1,6 +1,6 @@
 cities = []
 input_city = input("enter five cities: ")
-for c in range[:5]:
+for c in range(5):
     input_city = input("enter five cities: ")
     cities.append(input_city)
 for d in cities:
