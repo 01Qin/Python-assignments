27da74b4293219c54e3622fe2449a979d1accfe7
01Qin <sisukasqin@hotmail.com>
2023-10-19 11:03:01 +0800

    changed1

--- function 2.py
+++ function 2.py
@@ -1,4 +1,7 @@
-rolls = []
+import random
 number = int(input("how many dice do you want to roll: "))
+number_sum = 0
 for r in range(number):
-    print("the sum of the rolls is ", {r})
+    roll = random.randint(1,6)
+    number_sum += roll
+    print("the sum of the rolls is ", number_sum)
