a032ba6f63fed97f72b7cb9bb3a8dfee6175c139
01Qin <sisukasqin@hotmail.com>
2023-10-31 15:48:45 +0800

    changed1

---
+++ function 8.py
@@ -0,0 +1,21 @@
+def the_unit_price(diameter, price):
+    price_meter = diameter / 100 * 3.14 * price
+    return price_meter
+
+
+def main():
+    diameter_1 = float(input("please enter the first diameter: "))
+    price_1 = float(input("please enter the first price: "))
+    diameter_2 = float(input("please enter the second diameter: "))
+    price_2 = float(input("please enter the second price: "))
+
+    unit_price_1 = the_unit_price(diameter_1, price_1)
+    unit_price_2 = the_unit_price(diameter_2, price_2)
+
+    if unit_price_2 > unit_price_1:
+        print(f"the first pizza is {unit_price_1}, provides better value! ")
+    else:
+        print(f"the second pizza is{unit_price_2}, provides better value! ")
+
+
+main()
