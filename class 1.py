1340c2a82f62525430871bdcf43beeb58235d2f3
01Qin <sisukasqin@hotmail.com>
2023-11-20 20:50:03 +0800

    changed1

--- class 1.py
+++ class 1.py
@@ -1,4 +1,3 @@
-
 class Car:
     def __init__(self, registration_number, maximum_speed):
         self.registration_number = registration_number
@@ -6,12 +5,19 @@ class Car:
         self.current_speed = 0
         self.travelled_distance = 0
 
+    def accelerate(self, speed_change):
+        acc_speed = self.current_speed + speed_change
+        if acc_speed < 0:
+            self.current_speed = 0
+        elif acc_speed > self.maximum_speed:
+            self.current_speed = self.current_speed
 
-if __name__ == "main":
+
+if __name__ == "__main__":
     new_car = Car("ABC-123", 142)
 
     print("All the properties of the new car are:")
     print(f"Registration number is: {new_car.registration_number}.")
     print(f"Maximum speed is: {new_car.maximum_speed} km/h.")
-    print(f"Current_speed is: {new_car.current_speed} km/h.")
-    print(f"Travelled_distance is: {new_car.travelled_distance} km.")
+    print(f"Current speed is: {new_car.current_speed} km/h.")
+    print(f"Travelled distance is: {new_car.travelled_distance} km.")
