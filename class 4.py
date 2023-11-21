b3658f9cef84c7cfc01142df6f487cd528d42cf0
01Qin <sisukasqin@hotmail.com>
2023-11-21 20:25:54 +0800

    changed1

---
+++ class 4.py
@@ -0,0 +1,11 @@
+class Elevator:
+    def __init__(self, numbers_top, numbers_bottom):
+        self. numbers_top = numbers_top
+        self.numbers_bottom = numbers_bottom
+        self.new_elevator = numbers_bottom
+    def go_to_floor(self):
+
+    def floor_up(self):
+        print(f"the elevator is {}")
+    def floor_down(self):
+        print(f"the elevator is {}")
