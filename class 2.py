7eeac0fea73379be4ce7039bb432fef4a504bf7c
01Qin <sisukasqin@hotmail.com>
2023-11-24 14:55:15 +0800

    changed1

--- class 2.py
+++ class 2.py
@@ -1,22 +1,41 @@
 class Car:
     def __init__(self, registration_number, maximum_speed):
-        self.registration_number = registration_number
-        self.maximum_speed = maximum_speed
-        self.current_speed = 0
-        self.travelled_distance = 0
+        self._registration_number = str(registration_number)
+        self._maximum_speed = float(maximum_speed)
+        self._current_speed = 0.0
+        self._travelled_distance = 0.0
 
     def accelerate(self, speed_change):
-        acc_speed = self.current_speed + speed_change
+        acc_speed = self._current_speed + speed_change
         if acc_speed < 0:
-            self.current_speed = 0
-        elif acc_speed > self.maximum_speed:
-            self.current_speed = self.maximum_speed
+            self._current_speed = 0
+        elif acc_speed > self._maximum_speed:
+            self._current_speed = self._maximum_speed
         else:
-            self.current_speed = acc_speed
+            self._current_speed = acc_speed
 
     def drive(self, hours):
-        new_distance = self.current_speed * hours
-        self.travelled_distance += new_distance
+        new_distance = self._current_speed * hours
+        self._travelled_distance += new_distance
+
+    def stop(self):
+        self._current_speed = 0
+
+    @property
+    def registration_number(self):
+        return self._registration_number
+
+    @property
+    def maximum_speed(self):
+        return self._maximum_speed
+
+    @property
+    def current_speed(self):
+        return self._current_speed
+
+    @property
+    def travelled_distance(self):
+        return self._travelled_distance
 
 
 new_car = Car("ABC-123", 142)
@@ -24,6 +43,7 @@ new_car = Car("ABC-123", 142)
 new_car.accelerate(30)
 new_car.accelerate(70)
 new_car.accelerate(50)
+
 print("All the properties of the new car are:")
 print(f"Registration number is: {new_car.registration_number}.")
 print(f"Maximum speed is: {new_car.maximum_speed} km/h.")
