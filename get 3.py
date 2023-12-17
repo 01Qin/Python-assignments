ef9682555d1546cbf20d520b00fb7a7f788e3648
01Qin <sisukasqin@hotmail.com>
2023-12-17 15:37:20 +0800

    changed1

--- get 3.py
+++ get 3.py
@@ -1,26 +1,34 @@
 from flask import Flask, jsonify
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='localhost',
+    user='dbuser',
+    password='password',
+    database='airport',
+    autocommit=True
+)
 
 app = Flask(__name__)
 
-# Airport database (replace with your own implementation to fetch airport information)
-airport_database = {
-    "EFHK": {
-        "ICAO": "EFHK",
-        "Name": "Helsinki-Vantaa Airport",
-        "Location": "Helsinki"
-    },
-    "EGLL": {
-        "ICAO": "EGLL",
-        "Name": "Heathrow Airport",
-        "Location": "London"
-    },
-    # Add more airports as needed
-}
 
 @app.route("/airport/<icao_code>")
 def get_airport_info(icao_code):
-    if icao_code in airport_database:
-        airport_info = airport_database[icao_code]
+    cursor = connection.cursor(dictionary=True)
+    query = f"SELECT name, location FROM airport WHERE icao = '{icao_code}'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    cursor.close()
+
+    if result:
+        airport_info = {
+            "ICAO": icao_code,
+            "Name": result['name'],
+            "Location": result['location']
+        }
         return jsonify(airport_info)
     else:
         return jsonify({"error": "Airport not found"}), 404
+
+
+app.run(debug=True)
